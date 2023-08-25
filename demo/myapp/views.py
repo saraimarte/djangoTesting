524873ba6d5884980af21e8c14e492c7e8eba3f7
Sarai Marte <smartepujols@gmail.com>
2023-08-24 23:56:47 -0400

    c082423

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world")
+    return HttpResponse("this is my first django world")
