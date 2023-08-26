949e5d4aad1da0a32aad877fc46927ca2af9bfea
Sarai Marte <smartepujols@gmail.com>
2023-08-25 23:30:33 -0400

    database

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItem(models.ModeL):
+    title = models.CharField(max_Length = 200)
+    completed = models.BooleanField(defualt = False)
