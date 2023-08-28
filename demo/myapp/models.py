e669ff972daf41170392eab1950c12a998592ab6
Sarai Marte <smartepujols@gmail.com>
2023-08-28 17:41:07 -0400

    082823 still trying to use HTMX with Dj Not wrking

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
+#TodoItem(s) represents a database table
+#CRUD Create Retrieve Update Delete 
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Contact(models.Model):
+    name = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import TodoItem
+from django.db import models
 
+import json
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
-def todos(request):
-    items = TodoItem.objects.all()
-    return render(request, 'todos.html', {'todos': items })
-  
-def hello(request):
-    return render(request, 'hello.html')
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+ # Creating a new Todo
+def create_todo(request):
+    title = request.POST.get('title')
+    todo = Todo.objects.create(title=title)
+    todo.save()
+    todos = Todo.objects.all().order_by('-id')
+    return render(request, 'todo-list.html', {'todos': todos})
+
+# Marking completed True
+def mark_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.completed = True
+    todo.save()
+    todos = Todo.objects.all().order_by('-id')
+    return render(request, 'todo-list.html', {'todos': todos})
+
+# Deleting a Todo
+def delete_todo(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    todo.delete()
+    todos = Todo.objects.all().order_by('-id')
+    return render(request, 'todo-list.html', {'todos': todos})   
